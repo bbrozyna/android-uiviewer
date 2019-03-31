a186835eeb61bff1ba39b28d90de94cbe979d53e
bbrozyna <bbrozyna@gmail.com>
2019-04-01 00:55:27 +0200

    Resolved overstacking elements in ugly attributeKeys and attributeValues views

--- src/com/bbrozyna/caseweek/Controller.java
+++ src/com/bbrozyna/caseweek/Controller.java
@@ -4,6 +4,8 @@ import com.bbrozyna.caseweek.models.UIHierarchy;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +14,7 @@ public class Controller implements Initializable {
     public ListView attributeNames;
     public ListView attributeKeys;
     public ListView attributeValues;
+    public ImageView screenshot;
 
 
     @Override
@@ -19,10 +22,15 @@ public class Controller implements Initializable {
         UIHierarchy ui = new UIHierarchy("dump.xml");  // test file
         attributeNames.getItems().addAll(ui.getAllElementsNames());
         attributeNames.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> fillAttributes(ui, newValue));
+        Image image = new Image("https://img.gadgethacks.com/img/74/60/63656114260444/0/get-screenshot-editing-feature-from-android-pie-any-phone.w1456.jpg");
+        screenshot.setImage(image);
 
     }
 
     private void fillAttributes(UIHierarchy ui, Object newValue) {
+        System.out.println(newValue);
+        attributeValues.getItems().clear();
+        attributeKeys.getItems().clear();
         attributeKeys.getItems().addAll(ui.getAttributeByName().get(newValue).keySet());
         attributeValues.getItems().addAll(ui.getAttributeByName().get(newValue).values());
     }
